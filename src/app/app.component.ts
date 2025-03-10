3df0fce566b077c59efa810e719c4466c1215bdd
lea.kirotar <lea.pullerits@gmail.com>
2025-03-10 08:55:47 +0200

    Created infobar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
 import {SearchComponent} from './components/search/search.component';
 import {CalendarComponent} from './components/calendar/calendar.component';
+import {InfobarComponent} from './components/infobar/infobar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SearchComponent, CalendarComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchComponent, CalendarComponent, InfobarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
