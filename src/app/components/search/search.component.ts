c7b1b95978a470ce08683962c9ea489e4bdff39e
lea.kirotar <lea.pullerits@gmail.com>
2025-03-17 14:34:40 +0200

    Search bar necessary functions, interfaces etc.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import {AsyncPipe, CommonModule} from '@angular/common';
+import { Observable, BehaviorSubject, switchMap, finalize, of } from 'rxjs';
+import {Room, RoomSearch, SearchService} from './search.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  standalone: true,
+  imports: [AsyncPipe, FormsModule, CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  private searchSubject = new BehaviorSubject<RoomSearch | null>(null);
 
+  rooms$: Observable<Room[]>;
+  isLoading = false;
+  error: string | null = null;
+
+  searchData: RoomSearch = {
+    startDateTime: '',
+    endDateTime: '',
+    minCapacity: 0,
+    floor: 0,
+    keywords: ''
+  };
+
+  constructor(private searchService: SearchService) {
+    this.rooms$ = this.searchSubject.pipe(
+      switchMap(searchParams => {
+        if (!searchParams) return of([]);
+
+        this.isLoading = true;
+        this.error = null;
+
+        return this.searchService.searchRooms(searchParams)
+          .pipe(
+            finalize(() => {
+              this.isLoading = false;
+            })
+          );
+      })
+    );
+  }
+
+  onSubmit(): void {
+      this.searchRoom(this.searchData);
+  }
+
+  searchRoom(roomSearch: RoomSearch): void {
+    this.searchSubject.next(roomSearch);
+  }
 }
---
+++ src/app/components/search/search.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, catchError, of } from 'rxjs';
+import {environment} from '../../environments/environment';
+
+export interface Room {
+  id: number;
+  name: string;
+  number: string;
+  capacity: number;
+  floor: string;
+  facilities: string;
+  price: string;
+  keywords: string;
+}
+
+export interface RoomSearch {
+  startDateTime: string;
+  endDateTime: string;
+  minCapacity: number;
+  floor: number;
+  keywords: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  searchRooms(roomSearch: RoomSearch): Observable<Room[]> {
+    let params = new HttpParams();
+
+    if (roomSearch.startDateTime) {
+      params = params.set('startDateTime', roomSearch.startDateTime)
+    }
+    if (roomSearch.endDateTime) {
+      params = params.set('endDateTime', roomSearch.endDateTime)
+    }
+    if (roomSearch.minCapacity) {
+      params = params.set('minCapacity', roomSearch.minCapacity.toString());
+    }
+    if (roomSearch.floor) {
+      params = params.set('floor', roomSearch.floor.toString());
+    }
+    if (roomSearch.keywords) {
+      params = params.set('keywords', roomSearch.keywords);
+    }
+
+    return this.http.get<Room[]>(`${this.apiUrl}rooms/search`, { params })
+      .pipe(
+        catchError(error => {
+          console.error('Error searching for rooms:', error);
+          return of([]);
+        })
+      );
+  }
+}
