9f0fc4c83e6c4346dd7258756951db909b238054
lea.kirotar <lea.pullerits@gmail.com>
2025-03-20 12:08:38 +0200

    Added error handling, fixed api endpoint.

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import {catchError} from 'rxjs/operators';
+import {throwError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +13,16 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getRooms() {
-    return this.http.get(`${this.apiUrl}/all`);
+    return this.http.get(`${this.apiUrl}rooms/all`).pipe(
+      catchError(error => {
+        console.error('Error ruumide laadimisel:', error);
+        return throwError(() => new Error('Ei saanud ruume laadida. Proovi hiljem uuesti.'));
+      })
+    );
   }
 
-  searchRoom() {
-    return this.http.get(`${this.apiUrl}/search/`);
+searchRoom() {
+    return this.http.get(`${this.apiUrl}rooms/search/`);
   }
 
 }
