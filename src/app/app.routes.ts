256a282e309ce7f3d6d67760f6fb029e0a7104c2
lea.kirotar <lea.pullerits@gmail.com>
2025-03-20 15:45:05 +0200

    Added booking page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RoomPageComponent } from './components/room-page/room-page.component';
 import { LoginPageComponent} from './components/login-page/login-page.component';
+import {BookingPageComponent} from './components/booking-page/booking-page.component';
 
 export const routes: Routes = [
   { path: 'room-page/:id', component: RoomPageComponent },
-  { path: 'login-page', component: LoginPageComponent}
+  { path: 'login-page', component: LoginPageComponent},
+  { path: 'booking-page', component: BookingPageComponent}
 ];
