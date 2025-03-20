9436198d68594a242f0ee55a4a49a4aeec700a3a
lea.kirotar <lea.pullerits@gmail.com>
2025-03-20 15:32:02 +0200

    Added login route and changed rooms name.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RoomPageComponent } from './components/room-page/room-page.component';
+import { LoginPageComponent} from './components/login-page/login-page.component';
 
 export const routes: Routes = [
-  { path: 'room-page-component/:id', component: RoomPageComponent },
+  { path: 'room-page/:id', component: RoomPageComponent },
+  { path: 'login-page', component: LoginPageComponent}
 ];
