b87c7b11ed8fc3659813909391f616af068e5df3
lea.kirotar <lea.pullerits@gmail.com>
2025-03-21 10:36:26 +0200

    Added checking if room is available and fetching bookable rooms.

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {Observable, of, throwError} from 'rxjs';
+import {BookingSearch} from '../../components/main-calendar/main-calendar.component';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,30 @@ export class ApiService {
       catchError(error => {
         console.error('Error ruumide laadimisel:', error);
         return throwError(() => new Error('Ei saanud ruume laadida. Proovi hiljem uuesti.'));
+      })
+    );
+  }
+
+  getBookableRooms() {
+    return this.http.get(`${this.apiUrl}rooms/get-bookable-rooms`).pipe(
+      catchError(error => {
+        console.error('Error ruumide laadimisel:', error);
+        return throwError(() => new Error('Ei saanud ruume laadida. Proovi hiljem uuesti.'));
+      })
+    );
+  }
+
+  checkRoomAvailability (bookingSearch: BookingSearch): Observable<string> {
+    let params = new HttpParams()
+      .set('roomId', bookingSearch.roomId)
+      .set('startDateTime', bookingSearch.startTime)
+      .set('endDateTime', bookingSearch.endTime);
+
+    return this.http.get<boolean>(`${this.apiUrl}booking/get-room-availability`, {params}).pipe(
+      map(available => (available ? 'available' : 'booked')),
+      catchError(error => {
+        console.error('Error saadavuse laadimisel:', error);
+        return of('booked');
       })
     );
   }
