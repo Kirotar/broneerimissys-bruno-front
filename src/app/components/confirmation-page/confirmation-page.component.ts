9e60a5f979ad8738426a020e4fc7e54d011dbb05
lea.kirotar <lea.pullerits@gmail.com>
2025-03-21 09:18:41 +0200

    Created front page and confirmation page.

---
+++ src/app/components/confirmation-page/confirmation-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation-page',
+  imports: [],
+  templateUrl: './confirmation-page.component.html',
+  styleUrl: './confirmation-page.component.scss'
+})
+export class ConfirmationPageComponent {
+
+}
---
+++ src/app/components/front-page/front-page.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {CalendarComponent} from "../calendar/calendar.component";
+import {FooterComponent} from "../footer/footer.component";
+import {HeaderComponent} from "../header/header.component";
+import {InfobarComponent} from "../infobar/infobar.component";
+import {RoomsComponent} from "../rooms/rooms.component";
+import {SearchComponent} from "../search/search.component";
+
+@Component({
+  selector: 'app-front-page',
+    imports: [
+        CalendarComponent,
+        FooterComponent,
+        HeaderComponent,
+        InfobarComponent,
+        RoomsComponent,
+        SearchComponent
+    ],
+  templateUrl: './front-page.component.html',
+  styleUrl: './front-page.component.scss'
+})
+export class FrontPageComponent {
+
+}
