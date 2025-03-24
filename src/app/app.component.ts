3171b39b92f431b6bab87ab3c68f5ab21c06fdca
lea.kirotar <lea.pullerits@gmail.com>
2025-03-24 12:38:34 +0200

    Created more structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './core/layout/header/header.component';
-import {SearchComponent} from './features/home/components/search/search.component';
-import {InfobarComponent} from './features/home/components/infobar/infobar.component';
 import {FooterComponent} from './core/layout/footer/footer.component';
-import {RoomsComponent} from './features/home/components/rooms/rooms.component';
-import {MainCalendarComponent} from './features/home/components/main-calendar/main-calendar.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { RoomPageComponent } from './features/room/room-page/room-page.component';
-import { LoginPageComponent} from './components/login-page/login-page.component';
+import { LoginPageComponent} from './core/auth/login-page/login-page.component';
 import {BookingPageComponent} from './features/booking/booking-page/booking-page.component';
 import {FrontPageComponent} from './features/home/front-page/front-page.component';
 
