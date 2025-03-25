66634ece100ac393eba8c83b2b05d09c2a66eb8f
lea.kirotar <lea.pullerits@gmail.com>
2025-03-25 16:09:57 +0200

    Changed booking directive to accept array.

--- src/app/shared/booking-button.directive.ts
+++ src/app/shared/booking-button.directive.ts
@@ -1,20 +1,26 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import {Directive, HostListener, Input} from '@angular/core';
 import {Router} from '@angular/router';
+import {Booking} from '../features/home/components/main-calendar/main-calendar.component';
 
 @Directive({
   selector: '[appBookingButton]'
 })
 export class BookingButtonDirective {
   @Input() bookingRoute: string = '/booking';
-  @Input() roomId!: number;
-  @Input() userId?: number;
-  @Input() startDateTime!: string;
-  @Input() endDateTime!: string;
-  constructor(private router: Router) { }
+  @Input() bookings: Booking[] = [];
+
+  constructor(private router: Router) {
+  }
 
   @HostListener('click')
   navigateToBooking() {
-    this.router.navigate([this.bookingRoute], {queryParams: {roomId: this.roomId, userId: this.userId,
-        startDateTime: this.startDateTime, endDateTime: this.endDateTime}});
+    const bookingsArray = this.bookings;
+    if (bookingsArray.length > 0) {
+      this.router.navigate([this.bookingRoute], {
+        queryParams: {
+          bookings: JSON.stringify(bookingsArray)
+        }
+      });
+    }
   }
 }
