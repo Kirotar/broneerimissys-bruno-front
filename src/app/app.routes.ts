6a295635643c0d5383ecdc6e237963f4233b629f
lea.kirotar <lea.pullerits@gmail.com>
2025-03-26 11:19:31 +0200

    Created drafts of login and registration pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RoomPageComponent } from './features/room/room-page/room-page.component
 import { LoginPageComponent} from './core/auth/login-page/login-page.component';
 import {BookingPageComponent} from './features/booking/booking-page/booking-page.component';
 import {FrontPageComponent} from './features/home/front-page/front-page.component';
+import {RegistrationPageComponent} from './core/auth/registration-page/registration-page.component';
 
 export const routes: Routes = [
   { path: '', component: FrontPageComponent },
   { path: 'room-page/:id', component: RoomPageComponent },
-  { path: 'login', component: LoginPageComponent},
+  { path: 'login-page', component: LoginPageComponent},
+  { path: 'registration-page', component: RegistrationPageComponent},
   { path: 'booking', component: BookingPageComponent}
 ];
