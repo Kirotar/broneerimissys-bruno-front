7287b2dc657f5a5604e815d07c4f0bfc776bba5d
lea.kirotar <lea.pullerits@gmail.com>
2025-03-26 12:05:37 +0200

    Added basic draft and design for confirmation page.

--- src/app/features/booking/confirmation-page/confirmation-page.component.ts
+++ src/app/features/booking/confirmation-page/confirmation-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-confirmation-page',
-  imports: [],
+    imports: [
+        RouterLinkActive
+    ],
   templateUrl: './confirmation-page.component.html',
   styleUrl: './confirmation-page.component.scss'
 })
