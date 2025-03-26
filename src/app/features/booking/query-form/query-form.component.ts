d260fb5e3feda3b7f9c9060cf8f853464cd05e69
lea.kirotar <lea.pullerits@gmail.com>
2025-03-26 12:41:45 +0200

    Added query form.

---
+++ src/app/features/booking/query-form/query-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-query-form',
+  imports: [],
+  templateUrl: './query-form.component.html',
+  styleUrl: './query-form.component.scss'
+})
+export class QueryFormComponent {
+
+}
