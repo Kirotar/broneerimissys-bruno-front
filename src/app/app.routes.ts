50b3962cf1e2933599af5f8d814bce1a84cb6fde
lea.kirotar <lea.pullerits@gmail.com>
2025-03-26 12:05:48 +0200

    Added routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginPageComponent} from './core/auth/login-page/login-page.component';
 import {BookingPageComponent} from './features/booking/booking-page/booking-page.component';
 import {FrontPageComponent} from './features/home/front-page/front-page.component';
 import {RegistrationPageComponent} from './core/auth/registration-page/registration-page.component';
+import {ConfirmationPageComponent} from './features/booking/confirmation-page/confirmation-page.component';
 
 export const routes: Routes = [
   { path: '', component: FrontPageComponent },
-  { path: 'room-page/:id', component: RoomPageComponent },
-  { path: 'login-page', component: LoginPageComponent},
-  { path: 'registration-page', component: RegistrationPageComponent},
-  { path: 'booking', component: BookingPageComponent}
+  { path: 'room/:id', component: RoomPageComponent },
+  { path: 'login', component: LoginPageComponent},
+  { path: 'registration', component: RegistrationPageComponent},
+  { path: 'booking', component: BookingPageComponent},
+  { path: 'confirmation', component: ConfirmationPageComponent}
 ];
