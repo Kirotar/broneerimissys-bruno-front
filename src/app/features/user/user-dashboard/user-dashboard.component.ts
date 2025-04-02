01928801e5c03b541d93312a30a3d03bf88e8c6c
lea.kirotar <lea.pullerits@gmail.com>
2025-04-02 13:05:22 +0300

    Added user dashboard.

--- src/app/features/user/user-dashboard/user-dashboard.component.ts
+++ src/app/features/user/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService} from '../user.service';
+import { User} from '../user.model';
 
 @Component({
   selector: 'app-user-page',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './user-dashboard.component.scss'
 })
 export class UserDashboardComponent {
+  user: User | null = null;
+  showPasswordFields: boolean = true;
+
+  constructor (private userService: UserService) {}
+
+  ngOnInit() {
+    this.fetchUserInfo();
+  }
+
+   fetchUserInfo(){
+    this.userService.fetchUserInfo().subscribe((data: User) => {
+      this.user = data;
+    });
+}
+
+  togglePasswordFields(): void {
+    this.showPasswordFields = !this.showPasswordFields;
+  }
 
 }
